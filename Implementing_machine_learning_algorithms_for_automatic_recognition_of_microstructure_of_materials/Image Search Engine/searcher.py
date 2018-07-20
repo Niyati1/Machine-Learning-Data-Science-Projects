17b04c001b67342bcb5e515133ca7ba07323aa20
Niyati1 <shahniyati1@gmail.com>
2018-07-20 11:20:22 -0700

    Update searcher.py

--- Implementing_machine_learning_algorithms_for_automatic_recognition_of_microstructure_of_materials/Image Search Engine/searcher.py
+++ Implementing_machine_learning_algorithms_for_automatic_recognition_of_microstructure_of_materials/Image Search Engine/searcher.py
@@ -1,9 +1,9 @@
+# import necessary libraries
 import numpy as np
 import csv
 
 class Searcher:
     def __init__(self, indexPath):
-        
         self.indexPath = indexPath
 
     # The search function search for visually similar micrographs, in the csv file where the histogram of all the micrographs are saved.
@@ -12,20 +12,18 @@ class Searcher:
         results = {}        
         with open(self.indexPath) as f:            
             reader = csv.reader(f)
-
-            for row in reader:
-                features = [float(x) for x in row[1:]]
-                #the histograms are compared using euclidan squared distance
+            for r in reader:
+                features = [float(x) for x in r[1:]]
+                #histogram of input microstructure is compared with hostograms of all other microstructures using chi-squared error distance.
                 d = self.euc_distance(features, queryFeatures)
-                results[row[0]] = d
+                results[r[0]] = d
                 f.close()
-
+        # the distance or error are sorted in ascending order, such that microstructures having the lowest error/distance compared to the input microstructure are placed first in results {} array
         results = sorted([(v, k) for (k, v) in results.items()])
-
+        # first 6 micrographs are returned as visually similar micrographs with least error.
         return results[:limit]
 
     def euc_distance(self, histA, histB, eps = 1e-10):
         d = 0.5 * np.sum([((a - b) ** 2) / (a + b + eps)
         for (a, b) in zip(histA, histB)])
-
         return d
