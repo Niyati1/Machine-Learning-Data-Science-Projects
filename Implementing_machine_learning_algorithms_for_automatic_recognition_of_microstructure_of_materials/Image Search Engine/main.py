36d4317c03f3c3edd04a2cf84f0640f89e28ebb5
Niyati1 <shahniyati1@gmail.com>
2018-07-20 12:20:06 -0700

    Update main.py

--- Implementing_machine_learning_algorithms_for_automatic_recognition_of_microstructure_of_materials/Image Search Engine/main.py
+++ Implementing_machine_learning_algorithms_for_automatic_recognition_of_microstructure_of_materials/Image Search Engine/main.py
@@ -1,7 +1,9 @@
+#import necessary libraries
+
 import random, os
 import tkinter
-from search.colordescriptor import Descriptor
-from search.searcher import Searcher
+import Descriptor
+import searcher
 import argparse
 from tkinter import *
 from tkinter import filedialog
@@ -20,70 +22,85 @@ import os
 
 def main():
     
+    #this model is imported so that the resnet50 model can be used as a feature extractor for a input micrograph and then predict the type of micrograph with the trained Model 4 for both Task1 and Task2
     model = applications.resnet50.ResNet50(weights='imagenet', include_top=False, pooling='avg')
+    # model_task_1 and model_task_2 is nothing but Model 4 (model which gave the best accuracy for task1 and task2) for task 1 and task 2 respectively.
     m = load_model('model_task_1.h5')
     m_f = load_model('model_task_2.h5')
 
     print('loaded')
     window = Tk()
- 
+    #create a tkinter window, which will have 3 buttons to perform 3 fuctions:
+    #1. Predict the type of microstructure.
+    #2. Search microstructures by keyword.
+    #3. Search visually similar microstructures.
     window.title("Image Search Engine")
     
     window.geometry('350x200')
-    
+    #This function is to return the string value for a predicted integer value by Model 4
+    def getText(text,text_f):
+        
+        if(text==0):
+            s = "Type of Material is Ceramic"
+        elif(text==1):
+            s = "Type of Material is Composite"
+        elif(text==2):
+            s = "Type of Material is Iron"
+        elif(text==3):
+            s = "Type of Material is Aluminium"
+        elif(text==4):
+            s = "Type of Material is Copper"
+        elif(text==5):
+            s = "Type of Material is Magnesium"
+        elif(text==6):
+            s = "Type of Material is Misc"
+        elif(text==7):
+            s = "Type of Material is Refactory"            
+        elif(text==8):
+            s = "Type of Material is SuperAlloy"
+        elif(text==9):
+            s = "Type of Material is Titanium"
+        elif(text==10):
+            s = "Type of Material is Steel"     
+        
+        if(text_f==0):
+            s_f = "Ferrous"
+        elif(text_f==1):
+            s_f = "Non Ferrous"
+        elif(text_f==2):
+            s_f = "Others"
+            
+        return s, s_f    
+        
+    #this function implements working of 1st function (Predict the type of microstructure)
     def Clicked1():
         
         in_path = filedialog.askopenfilename()
         print (in_path)
 
         root = Toplevel()
-        
+        #the input microstructure image is conveted into dimension of 224 by 224 as required by the Resnet50 architecture0
         img = image.load_img(in_path, target_size=(224, 224))
         if img is not None:
+            #this conversion is used to print the rounded value of the predicted probability score.
             np.set_printoptions(suppress=True,formatter={'float_kind':'{:f}'.format})
+            #the features of input microstructure are converted into arrays
             x = image.img_to_array(img)
             x = np.expand_dims(x, axis=0)
             x = preprocess_input(x)
             features = model.predict(x)[0]
             features = features.reshape(1,2048)
+            # p is the prediction value by a model
+            # k is the prediction score by a model
             p = m.predict_classes(features)
             k = m.predict(features)
             p_f = m_f.predict_classes(features)
             k_f = m_f.predict(features)
         im = PIL.Image.open(in_path).resize((300,300))
         photo = PIL.ImageTk.PhotoImage(im)
+        s, s_f = getText(p[0],p_f[0])
         
         
-        if(p[0]==0):
-            s = "Type of Material is Ceramic"
-        elif(p[0]==1):
-            s = "Type of Material is Composite"
-        elif(p[0]==2):
-            s = "Type of Material is Iron"
-        elif(p[0]==3):
-            s = "Type of Material is Aluminium"
-        elif(p[0]==4):
-            s = "Type of Material is Copper"
-        elif(p[0]==5):
-            s = "Type of Material is Magnesium"
-        elif(p[0]==6):
-            s = "Type of Material is Misc"
-        elif(p[0]==7):
-            s = "Type of Material is Refactory"            
-        elif(p[0]==8):
-            s = "Type of Material is SuperAlloy"
-        elif(p[0]==9):
-            s = "Type of Material is Titanium"
-        elif(p[0]==10):
-            s = "Type of Material is Steel"     
-        
-        if(p_f[0]==0):
-            s_f = "Ferrous"
-        elif(p_f[0]==1):
-            s_f = "Non Ferrous"
-        elif(p_f[0]==2):
-            s_f = "Others"
-            
         pr = round((k[0][p[0]] * 100),3)
         prob = str(pr) 
         
@@ -94,6 +111,7 @@ def main():
         
         label.image = photo  
     
+    #this function is for implementing 2nd feature of image search engine (search microstructrues by keywords)
     def Clicked2():         
         
         window = Tk()
@@ -112,7 +130,7 @@ def main():
             t = None
             root = Toplevel()
             root.title("Search Results for "+ txt.get() + " Images..." )
-    
+            
             if (txt.get()=="ceramic"):
                 t = 0
             elif(txt.get()=="composite"):
@@ -138,9 +156,11 @@ def main():
             
             path="dataset/"
           
+            #this is while loop is to search for only 3 similar microsrtucture. If 3 microstructure for a given keyword is found then the while loop is exited.
             while(i<=2):
+                # the code randomly picks a microstructure image from a pool of all microstructures of different type
                 random_filename = random.choice([x for x in os.listdir(path) if os.path.isfile(os.path.join(path, x))])
-                
+                # this extracts the feature of selected microstructure and predict its type
                 img = image.load_img(os.path.join(path, random_filename), target_size=(224, 224))
                 if img is not None:
                     x = image.img_to_array(img)
@@ -150,6 +170,7 @@ def main():
                     features = features.reshape(1,2048)
                     p = m.predict_classes(features)
                     k = m.predict(features)
+                    #if the predicted type is same as the searched text, then it is added to an array else a new random microstructure is selected again
                     if (p[0]==t):
                         i = i+1
                         image_array.append(os.path.join(path, random_filename))
@@ -159,6 +180,7 @@ def main():
             root.grid_columnconfigure(0, weight=1)
             cnv = Canvas(root)
             cnv.grid(row=0, column=0, sticky='nswe')
+            #this is the window scroll function
             hScroll = Scrollbar(root, orient=HORIZONTAL, command=cnv.xview)
             hScroll.grid(row=1, column=0, sticky='we')
             vScroll = Scrollbar(root, orient=VERTICAL, command=cnv.yview)
@@ -180,10 +202,8 @@ def main():
         
         btn = Button(window, text="Search",command = c2)
         btn.place(relx=0.5, rely=0.5, anchor=CENTER)
- 
         
-        
-    
+    # this fucntion implements 3rd feature of image search engine (search for visually similar microstructures)
     def Clicked3():
         
         cd = Descriptor((8, 12, 3))
@@ -193,7 +213,7 @@ def main():
 
         query = cv2.imread(in_path)
         features = cd.describe(query)
-
+        #histogram of the input microstructure is compared to histograms of all other microstructures/
         searcher = Searcher("compare_hist.csv")
         results = searcher.search(features)
         print(results)
@@ -227,37 +247,8 @@ def main():
                 p_f = m_f.predict_classes(features)
                 k = m.predict(features)
                 print(p[0])
+                t, s_f = getText(p[0],p_f[0])
                 
-            if(p[0]==0):
-                t = "Type of Material: Ceramic"
-            elif(p[0]==1):
-                t = "Type of Material: Composite"
-            elif(p[0]==2):
-                t = "Type of Material: Iron"
-            elif(p[0]==3):
-                t = "Type of Material: Aluminium"
-            elif(p[0]==4):
-                t = "Type of Material: Copper"
-            elif(p[0]==5):
-                t = "Type of Material: Magnesium"
-            elif(p[0]==6):
-                t = "Type of Material: Misc"
-            elif(p[0]==7):
-                t = "Type of Material: Refactory"            
-            elif(p[0]==8):
-                t = "Type of Material: SuperAlloy"
-            elif(p[0]==9):
-                t = "Type of Material: Titanium"
-            elif(p[0]==10):
-                t = "Type of Material: Steel"     
-        
-            if(p_f[0]==0):
-                s_f = "Ferrous"
-            elif(p_f[0]==1):
-                s_f = "Non Ferrous"
-            elif(p_f[0]==2):
-                s_f = "Others"
-            
             nn= str(num)
             im = PIL.Image.open(s).resize((200,200))   
             tkimage =  PIL.ImageTk.PhotoImage(im)
